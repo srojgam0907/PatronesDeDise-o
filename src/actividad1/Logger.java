80a1481d0d5fb31f5408a093d7437d460848d424
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-09 10:51:37 +0200

    Clase Logger

--- src/actividad1/Logger.java
+++ src/actividad1/Logger.java
@@ -1,8 +1,26 @@
 package actividad1;
 
+import java.util.List;
+
 public class Logger {
 
-	private static Logger instance;
+	private static Logger logger; 
+	private List<String> mensajes; 
 	
+	private Logger() {}
 	
+	public static Logger getLogger() { 
+        if (logger == null) {
+            logger = new Logger();
+        }
+        return logger; 
+    }
+	
+	public void registrar(String mensaje) {
+		mensajes.add(mensaje);
+	}
+	
+	public void mostrarLogs() {
+		mensajes.forEach(System.out::println); 
+	}
 }
