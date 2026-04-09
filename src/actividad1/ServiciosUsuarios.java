708e15ed0e539aacfb56470745bf797be8bd5814
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-09 11:07:13 +0200

    Clase ServiciosUsuarios terminada

--- src/actividad1/ServiciosUsuarios.java
+++ src/actividad1/ServiciosUsuarios.java
@@ -2,4 +2,11 @@ package actividad1;
 
 public class ServiciosUsuarios {
 
+	Logger log= Logger.getLogger();
+	
+	public ServiciosUsuarios() {}
+	
+	public void registrar(String mensaje) {
+		log.registrar(mensaje);
+	}
 }
