018bcc82d77a9a5df43e6b16e03f8cc313c1b501
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-09 11:06:43 +0200

    Clase SeervicioPedidos terminada

--- src/actividad1/ServicioPedidos.java
+++ src/actividad1/ServicioPedidos.java
@@ -2,4 +2,11 @@ package actividad1;
 
 public class ServicioPedidos {
 
+	Logger log= Logger.getLogger();
+	
+	public ServicioPedidos() {}
+	
+	public void registrar(String mensaje) {
+		log.registrar(mensaje);
+	}
 }
